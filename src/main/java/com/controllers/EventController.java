fb75f3c81ae4e56868370c7760630dd56aa81a74
SerhiiS <SerhiiS@softwareplanet.co.us>
2016-04-13 12:26:09 +0300

    add redirect to controller

--- src/main/java/com/controllers/EventController.java
+++ src/main/java/com/controllers/EventController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -22,13 +23,12 @@ public class EventController {
     UserService userService;
 
     @RequestMapping(value = "/addResponsibleUser",method = RequestMethod.POST)
-    public @ResponseBody String setResponsibleUser(HttpSession session, @RequestParam("eventId") int evetnId){
+    public String setResponsibleUser(HttpServletRequest req, HttpSession session, @RequestParam("eventId") int evetnId){
 
         int userId = (int) session.getAttribute("userId");
         Event eventById = eventService.getEventById(evetnId);
         eventById.setUser(userService.getUser(userId));
         eventService.update(eventById);
-
-        return "ok";
+        return "redirect:/events/";
     }
 }
