3f92c75d18724ab24ff4c61aea5f7f18b57a3ad0
Glazkov Victor <ViktorG@softwareplanet.uk.com>
2016-05-06 19:19:39 +0300

    dao for new entity

--- src/main/java/com/dataLayer/DAO/OrderDAO.java
+++ src/main/java/com/dataLayer/DAO/OrderDAO.java
@@ -1,49 +1,25 @@
 package com.dataLayer.DAO;
 
-import com.model.Entity.Event;
-import com.model.Entity.Item;
 import com.model.Entity.Order;
-import com.model.Entity.User;
 
 import java.util.List;
 
 public interface OrderDAO {
-    public void save(Order order);
+    void save(Order order);
 
-    Order getOrderFromOrderList(Order order);
+    Order getOrderByEventIdAndRestaurantIdAndPayerId(int eventId, int restaurantId, int payerId);
 
-    Order getOrderFromOrderList(int userId, int itemId, int eventId);
+    Order getOrdersByEventIdAndRestaurantId(int eventId, int restaurantId);
 
-    void updateAmount(Order order, int amount);
+    Order getOrderByOrderId(int orderId);
 
-    void addAmount(Order order, int amount);
+    List<Order> getOrdersByEventId(int eventId);
 
-    void updateAmount(Order order);
+    List<Order> getOrdersByRestaurantId(int restaurantId);
 
-    List<Order> getUserResponsibilityOrderList(Order order);
+    void setPayerForOrder(int orderId, int payerId);
 
-    public List<Order> orderListOfUserByEvent(int userId, int eventId);
-
-    List<Order> orderListOfUserByRestaurantInEvent(int userId, int eventId, int restaurantId);
-
-    public List<Order> orderListOfEvent(int eventId);
-
-    public void deleteItemFromOrder(int userId, int eventId, int itemId);
-
-    List<Order> selectOrderList(int userId, int eventId, int itemId);
-
-    List<Order> selectOrderList(int userId, int eventId);
-
-    void deleteOneItemFromOrder(int userId, int eventId, int itemId);
-
-    void deleteOneItemFromOrder(Order order);
-
-    public void updateOrderedOfOrder(boolean ordered, int eventId, int itemId);
-
-    void setResponsibleUser(int userId, int eventId, int restaurantId);
-
-    void addOneItemToOrder(User user, Item item, Event event, int number);
-
-    void deleteNumberItemFromOrder(int userId, int eventId, int itemId, int number);
+    void setPayerForOrder(int eventId, int restaurantId, int payerId);
 }
+
 
