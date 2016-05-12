a6669d98c3eded06a9f0cda399f8102a64cdc118
Serhii Shemshur <serhiis@softwareplanet.uk.com>
2016-05-12 10:20:41 +0300

    create spring context test config

---
+++ src/test/java/com/config/TestWebAppConfig.java
@@ -0,0 +1,12 @@
+package com.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com")
+public class TestWebAppConfig extends WebMvcConfigurerAdapter {
+}
