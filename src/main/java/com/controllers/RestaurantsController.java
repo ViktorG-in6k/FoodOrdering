6fa25dfb3881defd72f82df4f0773d8794620750
Glazkov Victor <ViktorG@softwareplanet.uk.com>
2016-05-12 10:23:33 +0300

    RESTful style URL in the RestaurantController

--- src/main/java/com/controllers/RestaurantsController.java
+++ src/main/java/com/controllers/RestaurantsController.java
@@ -1,17 +1,13 @@
 package com.controllers;
 
-import com.DTOLayer.DTOEntity.RequestRestaurantDTO;
 import com.DTOLayer.DTOEntity.restaurantDTO.RestaurantDTO;
 import com.serviceLayer.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
@@ -20,34 +16,18 @@ public class RestaurantsController {
     @Autowired
     RestaurantService restaurantService;
 
-    @RequestMapping(value = "/new_restaurant", method = RequestMethod.POST)
-    public String newRestaurant(HttpServletRequest req, @RequestParam("name")String name, @RequestParam("eventId") int eventId) throws UnsupportedEncodingException {
+    @RequestMapping(value = "/restaurants", method = RequestMethod.POST)
+    public String newRestaurant(HttpServletRequest req, @RequestParam("name") String name, @RequestParam("eventId") int eventId) throws UnsupportedEncodingException {
         restaurantService.saveByRequest(req);
         String ref = req.getHeader("Referer");
         return "redirect:" + ref + "#/" + eventId;
     }
 
-    @RequestMapping(value = "/events/event_{id}")
-    public String getAllRestaurants(HttpSession session, @PathVariable("id") String id) {
-        session.setAttribute("eventId", id);
-        session.setAttribute("allRestaurants", restaurantService.getListOfAllRestaurant());
-        session.setAttribute("backPage", "/events");
-        return "app";
-    }
-
-    @RequestMapping(value = "/events/event_{event}/restaurant_{id}")
-    public String getAllItemsByRestaurant(HttpSession session, @PathVariable("event") int event, @PathVariable("id") int id) {
-        session.setAttribute("restaurant", restaurantService.getRestaurantById(id));
-        session.setAttribute("Menu", restaurantService.getRestaurantById(id).getItem());
-        session.setAttribute("backPage", "/events/event_" + event);
-        return "menu";
-    }
-
-    @RequestMapping("/event_{event}/restaurants")
+    @RequestMapping("/events/{eventId}/restaurants")
     public
     @ResponseBody
     List<RestaurantDTO> getRestaurants(Authentication authentication,
-                                       @PathVariable("event") int eventId) {
+                                       @PathVariable("eventId") int eventId) {
         return restaurantService.getResponseListOfAllRestaurantsByEventId(eventId, authentication);
     }
 
@@ -58,15 +38,6 @@ public class RestaurantsController {
                                     @PathVariable("eventId") int eventId,
                                     @PathVariable("restaurantId") int restaurantId) {
         return restaurantService.getRestaurantDTOById(eventId, restaurantId, authentication);
-    }
-
-    @RequestMapping(value = "/event_{event}/update_restaurant_name", method = RequestMethod.POST)
-    public ResponseEntity<RestaurantDTO> updateRestaurantName(Authentication authentication,
-                                                              @PathVariable("event") int eventId,
-                                                              @RequestBody RequestRestaurantDTO restaurant) {
-        restaurantService.updateRestaurantName(restaurant);
-        RestaurantDTO restaurantDTO = getRestaurantById(authentication, eventId,restaurant.getId());
-        return new ResponseEntity<>(restaurantDTO, HttpStatus.OK);
     }
 }
 
