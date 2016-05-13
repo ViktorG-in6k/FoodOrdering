eeada56eb352886d6b32f455fb59b50ceb666a24
Glazkov Victor <ViktorG@softwareplanet.uk.com>
2016-05-13 16:37:40 +0300

    bind new service with controller

--- src/main/java/com/controllers/SplitBillController.java
+++ src/main/java/com/controllers/SplitBillController.java
@@ -1,10 +1,7 @@
 package com.controllers;
 
-import com.dataLayer.entity.base.User;
 import com.serviceLayer.googleAuthentication.CurrentUserDetails;
 import com.serviceLayer.service.UserService;
-import com.dataLayer.entity.external.SplitBillApi;
-import com.dataLayer.splitBillDTO.product.ProductRequestJSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -12,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 
 @Controller
 public class SplitBillController {
@@ -21,19 +17,6 @@ public class SplitBillController {
 
     @RequestMapping(value = "/test")
     public @ResponseBody String addBillToEvent(Authentication authentication) throws IOException {
-        SplitBillApi splitBillApi = new SplitBillApi();
-
-        User user = userService.getUser(getCurrentUserId(authentication));
-        splitBillApi.login(user.getEmail());
-
-        splitBillApi.newBill(831);
-
-        ProductRequestJSON productRequestJSON = new ProductRequestJSON(5,"banasssn",new BigDecimal(14.55));
-
-        splitBillApi.newProduct(productRequestJSON);
-        productRequestJSON = new ProductRequestJSON(5,"aaa",new BigDecimal(14.55));
-
-        splitBillApi.newProduct(productRequestJSON);
         return "OK";
     }
 
