ea2a5ec76d76dd1ad57846d1b9f9e40ff7592eb1
Serhii Shemshur <serhiis@softwareplanet.uk.com>
2016-05-15 15:01:00 +0300

    clean space add rest path to create restaurant

--- src/main/java/com/controllers/OrderController.java
+++ src/main/java/com/controllers/OrderController.java
@@ -6,50 +6,39 @@ import com.serviceLayer.googleAuthentication.CurrentUserDetails;
 import com.serviceLayer.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
-@Controller
+@RestController
 public class OrderController {
     @Autowired
     OrderService orderService;
 
     @RequestMapping(value = "/orders/{orderId}/responsibility", method = RequestMethod.POST)
-    public String setResponsibleUser(HttpServletRequest req, Authentication authentication,
-                                     @PathVariable("orderId") int orderId) {
+    public String setResponsibleUser(Authentication authentication, @PathVariable("orderId") int orderId) {
         orderService.setPayerById(orderId, getCurrentUserId(authentication));
-        String ref = req.getHeader("Referer");
-        return "redirect:" + ref;
+        return "{\"status\":\"200\"}";
     }
 
     @RequestMapping(value = "/orders/{orderId}/responsibility", method = RequestMethod.DELETE)
-    public String removeResponsibleUser(HttpServletRequest req,
-                                        @PathVariable("orderId") int orderId) {
+    public String removeResponsibleUser(@PathVariable("orderId") int orderId) {
         orderService.removePayer(orderId);
-        String ref = req.getHeader("Referer");
-        return "redirect:" + ref;
+        return "{\"status\":\"200\"}";
+    }
+
+    @RequestMapping(value = "orders/{orderId}/status", method = RequestMethod.PUT)
+    public String changeOrderStatus(@PathVariable int orderId, @RequestParam("status") Status status) throws IOException {
+        orderService.changeOrderStatus(orderId, status);
+        return "{\"status\":\"200\"}";
+    }
+
+    @RequestMapping(value = "api/orders/{id}", method = RequestMethod.GET)
+    public OrderDTO getOrderById(@PathVariable("id") int id) {
+        return new OrderDTO(orderService.getOrderById(id));
     }
 
     private int getCurrentUserId(Authentication authentication) {
         return ((CurrentUserDetails) authentication.getPrincipal()).getUser().getId();
-    }
-
-    @RequestMapping(value = "orders/{orderId}/status", method = RequestMethod.PUT)
-    public
-    @ResponseBody
-    String changeOrderStatus(@PathVariable int orderId, @RequestParam("status") Status status) throws IOException {
-        orderService.changeOrderStatus(orderId, status);
-        return "{\"status\":\"ok\"}";
-    }
-
-    @RequestMapping(value = "api/orders/{id}", method = RequestMethod.GET)
-    public
-    @ResponseBody
-    OrderDTO getOrderById(@PathVariable("id") int id) {
-        return new OrderDTO(orderService.getOrderById(id));
     }
 }
 
--- src/main/java/com/controllers/RestaurantsController.java
+++ src/main/java/com/controllers/RestaurantsController.java
@@ -1,40 +1,32 @@
 package com.controllers;
 
 import com.dataLayer.entity.DTO.restaurantDTO.RestaurantDTO;
+import com.dataLayer.entity.base.Restaurant;
 import com.serviceLayer.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.UnsupportedEncodingException;
 import java.util.List;
 
-@Controller
+@RestController
 public class RestaurantsController {
     @Autowired
     RestaurantService restaurantService;
 
-    @RequestMapping(value = "/restaurants", method = RequestMethod.POST)
-    public String newRestaurant(HttpServletRequest req, @RequestParam("name") String name, @RequestParam("eventId") int eventId) throws UnsupportedEncodingException {
-        restaurantService.saveByRequest(req);
-        String ref = req.getHeader("Referer");
-        return "redirect:" + ref + "#/" + eventId;
+    @RequestMapping(value = "api/restaurants", method = RequestMethod.POST)
+    public String newRestaurant(@RequestParam("name") String name, @RequestParam("link") String link, @RequestParam("phone") String phone){
+        restaurantService.save(new Restaurant(name, phone, link));
+        return "{\"status\":\"200\"}";
     }
 
     @RequestMapping("/events/{eventId}/restaurants")
-    public
-    @ResponseBody
-    List<RestaurantDTO> getRestaurants(Authentication authentication,
+    public List<RestaurantDTO> getRestaurants(Authentication authentication,
                                        @PathVariable("eventId") int eventId) {
         return restaurantService.getResponseListOfAllRestaurantsByEventId(eventId, authentication);
     }
 
     @RequestMapping("/events/{eventId}/restaurants/{restaurantId}")
-    public
-    @ResponseBody
-    RestaurantDTO getRestaurantById(Authentication authentication,
+    public RestaurantDTO getRestaurantById(Authentication authentication,
                                     @PathVariable("eventId") int eventId,
                                     @PathVariable("restaurantId") int restaurantId) {
         return restaurantService.getRestaurantDTOById(eventId, restaurantId, authentication);
