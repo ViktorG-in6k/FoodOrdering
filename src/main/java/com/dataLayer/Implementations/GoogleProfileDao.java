9c83feb7c22182bdbb8f6684590d3e0f12684520
Serhii Shemshur <serhiis@softwareplanet.uk.com>
2016-05-17 09:29:42 +0300

    Update GoogleProfileDao.java
    remove supervarning

--- src/main/java/com/dataLayer/Implementations/GoogleProfileDao.java
+++ src/main/java/com/dataLayer/Implementations/GoogleProfileDao.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-@SuppressWarnings("unchecked")
 @Repository
 @Transactional
 public class GoogleProfileDao {
