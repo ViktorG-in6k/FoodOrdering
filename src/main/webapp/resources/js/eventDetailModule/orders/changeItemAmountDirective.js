f6bc21a04e7d3a2c097c4ad37d98b979a1f25f2e
Serhii Shemshur <serhiis@softwareplanet.uk.com>
2016-05-19 15:43:41 +0300

    create changeOrderItemDerivctive

--- src/main/webapp/resources/js/eventDetailModule/orders/changeItemAmountDirective.js
+++ src/main/webapp/resources/js/eventDetailModule/orders/changeItemAmountDirective.js
@@ -1,18 +1,52 @@
 var directive = angular.module('orderItemAmount', []);
 
-directive.directive('changeItemAmountDirective', function() {
+directive.directive('changeItemAmountDirective', function () {
     return {
-        templateUrl:'/resources/js/eventDetailModule/orders/changeItemTemplate.html',
+        templateUrl: '/resources/js/eventDetailModule/orders/changeItemTemplate.html',
         restrict: 'E',
         scope: {
             amount: '=',
             itemId: '@',
-            orderId: '@'
+            orderId: '@',
+            itemName:'@'
         },
-        controller:function($scope){
-          console.log($scope.amount)
+        controller: function (OrderListService, $http, $scope, CommonOrderService) {
+
+            function updateUsersItemList() {
+                CommonOrderService.getItemList($scope.orderId, $scope.itemId).success(function (data) {
+                    $scope.itemList = data;
+                });
+            }
+
+            updateUsersItemList();
+
+            $scope.addOneItemToUserOrder = function (userId) {
+                OrderListService.addOneItemToUserOrder($scope.itemId, $scope.orderId, userId).success(function () {
+                    updateUsersItemList();
+                    $scope.amount = $scope.amount + 1;
+                })
+            };
+
+            $scope.removeOneItemFromUserOrder = function (userId) {
+                OrderListService.removeOneItemFromUserOrder($scope.itemId, $scope.orderId, userId).success(function () {
+                    updateUsersItemList();
+                    $scope.amount = $scope.amount - 1;
+                    if($scope.amount == 0){
+                        OrderListService.updateCommonOrder();
+                    }
+                })
+            };
+
+
+            $scope.removeItemFromUserOrder = function (userId, itemAmount) {
+                OrderListService.removeItemFromUserOrder($scope.orderId, $scope.itemId, userId).success(function(){
+                    updateUsersItemList();
+                    $scope.amount = $scope.amount - itemAmount;
+                    OrderListService.updateCommonOrder();
+                });
+            }
+
+
         }
     };
 });
-
-
