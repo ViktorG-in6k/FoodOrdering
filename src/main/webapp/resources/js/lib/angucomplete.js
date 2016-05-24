d7c70c2464e8deaa1888792729190c17140b452d
Serhii Shemshur <serhiis@softwareplanet.uk.com>
2016-05-24 16:13:52 +0300

    Merge branch 'autocomplite-inputs' of https://github.com/ViktorG-in6k/FoodOrderind into autocomplite-inputs
    Conflicts:
    	src/main/java/com/controllers/UserController.java

--- src/main/webapp/resources/js/lib/angucomplete.js
+++ src/main/webapp/resources/js/lib/angucomplete.js
@@ -23,7 +23,8 @@ angular.module('angucomplete', [] )
                 "localData": "=localdata",
                 "searchFields": "@searchfields",
                 "minLengthUser": "@minlength",
-                "matchClass": "@matchclass"
+                "matchClass": "@matchclass",
+                "searchStr":"=searchStr"
             },
             template: '<div class="angucomplete-holder"><input id="{{id}}_value" ng-model="searchStr" type="text" placeholder="{{placeholder}}" class="{{inputClass}}" onmouseup="this.select();" ng-focus="resetHideResults()" ng-blur="hideResults()" /><div id="{{id}}_dropdown" class="angucomplete-dropdown" ng-if="showDropdown"><div class="angucomplete-searching" ng-show="searching">Searching...</div><div class="angucomplete-searching" ng-show="!searching && (!results || results.length == 0)">No results found</div><div class="angucomplete-row" ng-repeat="result in results" ng-mousedown="selectResult(result)" ng-mouseover="hoverRow()" ng-class="{\'angucomplete-selected-row\': $index == currentIndex}"><div ng-if="imageField" class="angucomplete-image-holder"><img ng-if="result.image && result.image != \'\'" ng-src="{{result.image}}" class="angucomplete-image"/><div ng-if="!result.image && result.image != \'\'" class="angucomplete-image-default"></div></div><div class="angucomplete-title" ng-if="matchClass" ng-bind-html="result.title"></div><div class="angucomplete-title" ng-if="!matchClass">{{ result.title }}</div><div ng-if="result.description && result.description != \'\'" class="angucomplete-description">{{result.description}}</div></div></div></div>',
 
@@ -36,7 +37,7 @@ angular.module('angucomplete', [] )
                 $scope.searching = false;
                 $scope.pause = 500;
                 $scope.minLength = 3;
-                $scope.searchStr = null;
+                //$scope.searchStr = null;
 
                 if ($scope.minLengthUser && $scope.minLengthUser != "") {
                     $scope.minLength = $scope.minLengthUser;
